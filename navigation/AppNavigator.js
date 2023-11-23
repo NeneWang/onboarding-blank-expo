1c51d801524d95934f71a176615b8050d19fca3f
NelsonWang <wangnelson2@gmail.com>
2023-11-23 10:57:22 -0500

    boost json app navigatro

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen.js';
 import OnboardingScreen from '../screens/OnboardingScreen.js';
-import { getItem } from '../utils/asyncStorage.js';
 
 
 const Stack = createNativeStackNavigator();
@@ -12,8 +11,15 @@ const AppNavigator = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Onboarding">
-                <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Onboarding" component={OnboardingScreen} 
+                options={{
+                    headerShown: false
+                }} />
+                <Stack.Screen name="Home" component={HomeScreen} 
+                options={{
+                    headerShown: false
+                }}
+                />
             </Stack.Navigator>
         </NavigationContainer>
     );
